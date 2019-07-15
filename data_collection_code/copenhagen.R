52884e3a2ebf17271db3034cbda557c52b5de871
mglosinno <45425902+mglosinno@users.noreply.github.com>
2019-07-15 13:42:30 +0200

    The Copenhagen Scraper is finialised.
    This is also noted in the excel sheet.
    The structure of the municipality of Copenhagen, made it so that each item in all of the meetings have their own archive file. This is because each item had it's own webpage.

--- data_collection_code/copenhagen.R
+++ data_collection_code/copenhagen.R
@@ -35,41 +35,81 @@ while (x <= 320) { #320 = magic number
 
     #Getting the dates and links for the meetings of the page
     links <- url %>% html_nodes('#main-content .first a') %>% html_attr("href")
+    links <- links[1:10]
     links <- paste0("https://www.kk.dk", links)
     
-    dates <- url %>% html_nodes('.date-display-single') %>% html_text(trim = T)
+    #getting dates from the page before, to make sure that any meetings on the same day are taken into account
+    if(x > 1){
+    url2 <- paste0(start_url, x - 1) 
+    url2 <- read_html(url2)
+    url3 <- paste0(start_url, x - 2) 
+    url3 <- read_html(url3)
+    dates3 <- url %>% html_nodes('.date-display-single') %>% html_text(trim = T)
+    dates2 <- url2 %>% html_nodes('.date-display-single') %>% html_text(trim = T)
+    dates1 <- url3 %>% html_nodes('.date-display-single') %>% html_text(trim = T)
+    dates <- str_c(c(dates1[1:10], dates2[1:10], dates3[1:10]))
     dates <- gsub("[[:punct:]][[:space:]]", "-", dates)
+    dates <- make.unique(dates, "-")
+    dates <- dates[21:30]
+    } else{
+        if(x == 1){
+            url2 <- paste0(start_url, x - 1) 
+            url2 <- read_html(url2)
+            dates2 <- url %>% html_nodes('.date-display-single') %>% html_text(trim = T)
+            dates1 <- url2 %>% html_nodes('.date-display-single') %>% html_text(trim = T)
+            dates <- str_c(c(dates1, dates2))
+            dates <- gsub("[[:punct:]][[:space:]]", "-", dates)
+            dates <- make.unique(dates, "-")
+            dates <- dates[11-20]}
+        if(x == 0){
+        dates <- url %>% html_nodes('.date-display-single') %>% html_text(trim = T)
+        dates <- gsub("[[:punct:]][[:space:]]", "-", dates)
+        dates <- make.unique(dates, "-")}
+    }
     
     #Starting secondary loop over the different meetings
     while (y <= length(links)) {
       
         print(paste0("Working on link ", y, "/", length(links)))
         
-        file.name <- paste0("../data_archive/copenhagen_archive/", dates[y], ".RData")
-        
-        if (file.exists(file.name)){
-            #if archived file exists, load it instead of downloading again
-            load(file.name)
-        } else {
+     
       
         
-            to.save <- read_html(links[y])
+        ref_url <- read_html(links[y])
              
 
         #pull out the agenda items from meeting b
-        items <- to.save %>% html_nodes("td > a") %>% html_text(trim = T)
+        items <- ref_url %>% html_nodes("td > a") %>% html_text(trim = T)
         
         #pull out agenda item numbers for meeting b
-        nos <- to.save %>% html_nodes(".agenda-item-number") %>% html_text(trim = T)
+        nos <- ref_url %>% html_nodes(".agenda-item-number") %>% html_text(trim = T)
         
         #pull out links to each referat item
-        ref_links <- to.save %>% html_nodes("td > a") %>% html_attr("href")
+        ref_links <- ref_url %>% html_nodes("td > a") %>% html_attr("href")
         
         #Pull out each referat item and put it into a list, to then unlist the list
         refs <- list()
         for(lf in 1:length(nos)){
-            ref <- read_html(ref_links[lf])
-            ref <- ref %>% html_nodes(".sec-inner") %>% html_text(trim = T)
+            
+            file.name <- paste0("../data_archive/copenhagen_archive/", dates[y], " item ", nos[lf], ".RData")
+            
+            if (file.exists(file.name)){
+                #if archived file exists, load it instead of downloading again
+                load(file.name)
+            } else {
+            
+            agenda_link <- read_html(ref_links[lf])
+            agenda_link <- htmlParse(agenda_link)
+            agenda <- saveXML(agenda_link)
+            save(agenda, file = file.name)
+            }
+            
+            agenda <- htmlParse(agenda)
+            
+            ref <- xpathSApply(doc = agenda, 
+                                       path = "//*[contains(concat( ' ', @class, ' ' ), concat( ' ', 'sec-inner', ' ' ))]", 
+                               xmlValue)
+            
             ref <- str_squish(ref[5])
             ref <- sub("Del Tilgængelighed Udskriv Del på Facebook Linkedin share Del på Twitter E-mail ", "", ref)
             refs[[lf]] <- ref
@@ -77,7 +117,7 @@ while (x <= 320) { #320 = magic number
           refs <- unlist(refs)  
         
          #pull out date of meeting b
-        date <- to.save %>% html_nodes(".agenda-overview td") %>% html_text(trim = T)
+        date <- ref_url %>% html_nodes(".agenda-overview td") %>% html_text(trim = T)
         date <- str_extract(date, "[[:alnum:]]*[[:punct:]][[:alnum:]]*[[:punct:]][[:alnum:]]*")
         date <- as.Date(date, "%d-%m-%Y")
         
@@ -90,9 +130,7 @@ while (x <= 320) { #320 = magic number
             referat = refs,
             stringsAsFactors = F
         )
-        save(df, file = file.name)
-        
-        }
+
         #put that dataframe into our list of dataframes
         dfs[[length(dfs) + 1]] <- df
         
